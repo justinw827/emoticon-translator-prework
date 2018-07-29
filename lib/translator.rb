5c081e32d7bba3f8fc988ffc672d7a2489d89906
Justin Wang <justinw827@aol.com>
2018-07-29 19:40:33 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,6 +7,7 @@ def load_library(path)
   myEmotes = {"get_meaning" => {}, "get_emoticon" => {}}
   emotes.each do |key, val|
     myEmotes["get_meaning"] = {val[1] => key}
+    myEmotes["get_emoticon"] = {val[0] => val[1]}
     binding.pry
   end
 end
